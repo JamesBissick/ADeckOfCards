bb24f2501bb41057d77007b315b9da2132142613
James Bissick <bissickj@gmail.com>
2021-05-16 08:19:36 +0200

    🎨 - Refactor: Improved render in case scenarios, correct colors and placement for name 10

--- main.py
+++ main.py
@@ -24,13 +24,14 @@ class Card():
     def print_card(self):
         if self.suit == 'S':
             symbol = '♠'
+
         print(f"{self.name}{self.symbols[self.suit]}")
 
     def render(self, x, y, pen):
         # Draw the border
         pen.penup()
         pen.goto(x, y)
-        pen.color('blue')
+        pen.color('white')
         pen.goto(x - 50, y + 75)
         pen.begin_fill()
         pen.pendown()
@@ -42,21 +43,43 @@ class Card():
         pen.penup()
 
         # Draw suits in the middle
-        pen.color('white')
+        if self.symbols[self.suit] == '♦':
+            pen.color('red')
+        elif self.symbols[self.suit] == '♣':
+            pen.color('black')
+        elif self.symbols[self.suit] == '♥':
+            pen.color('red')
+        elif self.symbols[self.suit] == '♠':
+            pen.color('black')
+
+        # Draw suits in the middle
         pen.goto(x - 18, y - 30)
         pen.write(self.symbols[self.suit], False, font=('Courrier New', 48, 'normal'))
 
-        # Draw top left
-        pen.goto(x - 39, y + 47)
-        pen.write(self.name, False, font=('Courrier New', 18, 'normal'))
-        pen.goto(x - 40, y + 30)
-        pen.write(self.symbols[self.suit], False, font=('Courrier New', 18, 'normal'))
+        if self.name != '10':
+            # Draw top left
+            pen.goto(x - 39, y + 47)
+            pen.write(self.name, False, font=('Courrier New', 18, 'normal'))
+            pen.goto(x - 40, y + 30)
+            pen.write(self.symbols[self.suit], False, font=('Courrier New', 18, 'normal'))
 
-        # Draw bottom right
-        pen.goto(x + 31, y - 53)
-        pen.write(self.name, False, font=('Courrier New', 18, 'normal'))
-        pen.goto(x + 29, y - 71)
-        pen.write(self.symbols[self.suit], False, font=('Courrier New', 18, 'normal'))
+            # Draw bottom right
+            pen.goto(x + 31, y - 53)
+            pen.write(self.name, False, font=('Courrier New', 18, 'normal'))
+            pen.goto(x + 29, y - 71)
+            pen.write(self.symbols[self.suit], False, font=('Courrier New', 18, 'normal'))
+        else:
+            # Draw top left
+            pen.goto(x - 43, y + 47)
+            pen.write(self.name, False, font=('Courrier New', 18, 'normal'))
+            pen.goto(x - 40, y + 30)
+            pen.write(self.symbols[self.suit], False, font=('Courrier New', 18, 'normal'))
+
+            # Draw bottom right
+            pen.goto(x + 25, y - 53)
+            pen.write(self.name, False, font=('Courrier New', 18, 'normal'))
+            pen.goto(x + 29, y - 71)
+            pen.write(self.symbols[self.suit], False, font=('Courrier New', 18, 'normal'))
 
 
 class Deck():
@@ -73,11 +96,19 @@ class Deck():
     def shuffle(self):
         random.shuffle(self.cards)
 
+    def get_card(self):
+        card = self.cards.pop()
+        return card
 
 
+# Create deck and shuffle it
 deck = Deck()
 deck.shuffle()
-for card in deck.cards:
-    card.print_card()
+
+# Render 5 cards in the screen
+start_x = -250
+for x in range(5):
+    card = deck.get_card()
+    card.render(start_x + x * 125, 0, pen)
 
 window.mainloop()
